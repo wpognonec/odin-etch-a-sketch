724e0b353257b69a383a5b9f4363564878d2cc96
William Pognonec <william.pognonec@gmail.com>
2024-10-16 10:30:29 -0500

    Function to change grid size

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 
 let button = document.querySelector("button");
-// button.addEventListener("click")
+button.addEventListener("click", () => {
+  document.querySelector(".wrapper").remove()
+  const gridSize = parseInt(prompt("Please enter a grid size: "))
+  createGrid(gridSize)
+})
 
-for (let i = 1; i <= 256; i++) {
-  const div = document.createElement("div");
-  div.setAttribute("id", i)
-  div.className = "box"
-  div.style.width = `${960/16}px`
-  div.style.height = `${960/16}px`
-
-  // div.textContent = i
+function createGrid(size) {
+  const body = document.querySelector("body")
+  const wrapper = document.createElement("div");
+  wrapper.className = "wrapper"
+  for (let i = 1; i <= size*size; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("id", i)
+    div.className = "box"
+    div.style.width = `${960/size}px`
+    div.style.height = `${960/size}px`
+    
+    div.addEventListener("mouseenter", () => {
+      div.style.background = "slategray"
+    })
   
-
-  div.addEventListener("mouseenter", () => {
-    div.style.background = "slategray"
-  })
-
-  div.addEventListener("mouseleave", () => {
-    div.style.background = ""
-  })
-  document.querySelector(".wrapper").appendChild(div)
+    div.addEventListener("mouseleave", () => {
+      div.style.background = ""
+    })
+    wrapper.appendChild(div)
+  }
+  body.appendChild(wrapper)
 }
+
+createGrid(16)
