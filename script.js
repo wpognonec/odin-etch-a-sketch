49384ddbe09c1c966a58bc485d51f547b8843032
William Pognonec <william.pognonec@gmail.com>
2024-10-16 20:27:44 -0500

    Add color picker. Refactored some code.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let clearButton = document.querySelector(".clearGrid");
 let sizeDiv = document.querySelector(".sizeDiv");
-let input = document.querySelector("input");
+let input = document.querySelector("input#size");
+let color = document.querySelector("input#color")
 
 clearButton.addEventListener("click", () => {
   document.querySelector(".wrapper").remove()
@@ -20,6 +21,7 @@ function createGrid(size) {
   const body = document.querySelector("body")
   const wrapper = document.createElement("div");
   wrapper.className = "wrapper"
+  wrapper.setAttribute("oncontextmenu", "return false;")
   for (let i = 1; i <= size*size; i++) {
     const div = document.createElement("div");
     div.setAttribute("id", i)
@@ -28,14 +30,46 @@ function createGrid(size) {
     div.style.height = `${960/size}px`
     wrapper.appendChild(div)
   }
-  wrapper.addEventListener("mouseover", (e) => {
-    e.preventDefault()
-    if (e.buttons === 1) {
-      if (e.target.className !== "wrapper") {
-        e.target.style.backgroundColor = "slategray"
+
+  ["mousedown", "mouseover"].forEach(evt => {
+    wrapper.addEventListener(evt, (e) => {
+      if (e.buttons === 1) {
+        if (e.target.className !== "wrapper") {
+          e.target.style.backgroundColor = color.value
+        }
       }
-    }
+      if (e.buttons === 2) {
+        if (e.target.className !== "wrapper") {
+          e.target.style.backgroundColor = "white"
+        }
+      }
+    })
   })
+
+  // wrapper.addEventListener("mousedown", (e) => {
+  //   if (e.buttons === 1) {
+  //     if (e.target.className !== "wrapper") {
+  //       e.target.style.backgroundColor = color.value
+  //     }
+  //   }
+  //   if (e.buttons === 2) {
+  //     if (e.target.className !== "wrapper") {
+  //       e.target.style.backgroundColor = "white"
+  //     }
+  //   }
+  // })
+  // wrapper.addEventListener("mouseover", (e) => {
+  //   if (e.buttons === 1) {
+  //     if (e.target.className !== "wrapper") {
+  //       e.target.style.backgroundColor = color.value
+  //     }
+  //   }
+  //   if (e.buttons === 2) {
+  //     if (e.target.className !== "wrapper") {
+  //       e.target.style.backgroundColor = "white"
+  //     }
+  //   }
+  // })
   body.appendChild(wrapper)
 }
 
